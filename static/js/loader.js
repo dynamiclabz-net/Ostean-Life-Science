8575e8ef88f365ed337aa093115df341f80ba08d
devaasoni <sonidev774@gmail.com>
2026-04-06 15:17:21 +0530

    created & built osteon life science website

---
+++ static/js/loader.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+        document.getElementById("spinner").classList.add("hidden");
+    }, 1500); // Adjust timing if needed
+});
